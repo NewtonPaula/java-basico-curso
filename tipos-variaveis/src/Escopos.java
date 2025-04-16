ee479f02689113fa418f64e3bdefaf36cc442d13
Newton Paula <newtonpaula7@gmail.com>
2025-04-16 14:22:37 -0300

    um pouco sobre escopo

---
+++ tipos-variaveis/src/Escopos.java
@@ -0,0 +1,14 @@
+public class Escopos {
+    double salario = 1400;
+    public static void main(String[] args) {
+        Escopos escopos = new Escopos();
+        escopos.aumentar(20);
+    }
+
+    public void aumentar(double porcentagem){
+        salario = salario + (porcentagem/100 * salario);
+        System.out.println(salario);
+    }
+    
+}
+
